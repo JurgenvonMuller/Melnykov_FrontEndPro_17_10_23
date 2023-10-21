d6c742a30f70a1ff8747eb5e8c23bdab4ab60f55
Yuriy Melnykov <y.melnykov@outlook.com>
2023-10-21 07:39:35 +0200

    git add . --renormalize

---
+++ src/js/work2.js
@@ -0,0 +1,25 @@
+// var/ let/ const
+let  letVariable = 10;
+letVariable = 12;
+letVariable = 18;
+var varVariable = 10;
+const constVariable = 1;
+const PRIMARY_COLOR = '0000';
+// alert / confirm / prompt
+
+alert();
+console.log(); 
+// аналогия
+prompt();
+
+confirm ();
+
+//  return false or true
+
+// types / typeof / operator / operand
+
+// number string null boolean undefind
+
+// template literals 
+``
+``

5277b88e95c9b9e3f80470d6661dd39416ab24d8
Yuriy Melnykov <y.melnykov@outlook.com>
2023-10-21 06:43:31 +0200

    all close and sturt up to homework2

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,9 +1,1 @@
 "use strict";
-
-var a = 2;
-var b = 3;
-var c = Math.pow(a, b);
-console.log(c);
-a = c;
-console.log(c);
-console.log('123456');
--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,2 @@
-let a = 2;
-let b = 3;
-let c = a ** b;
-console.log(c);
-
-a = c;
-console.log(c);
-
-console.log('123456');
 
 

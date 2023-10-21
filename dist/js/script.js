5b872facc11da614f3ac1ae0f1b5271c0c48f61a
Yuriy Melnykov <y.melnykov@outlook.com>
2023-10-21 13:30:39 +0200

    homework 40%

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,10 +1,87 @@
 "use strict";
 
-var num = 123456;
-var firstNum = Number(num.toString(10)[4]);
-console.log(firstNum);
-if (firstNum == 1 || firstNum == 2 || firstNum == 3) {
-  console.log('yes');
-} else {
-  console.log('no');
-}
+function _typeof(o) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (o) { return typeof o; } : function (o) { return o && "function" == typeof Symbol && o.constructor === Symbol && o !== Symbol.prototype ? "symbol" : typeof o; }, _typeof(o); }
+/* 
+HOME TASK 2
+
+Tреба реалізувати просту програму з наступними умовами:
+
+За допомогою prompt запитати ім'я користувача.
+За допомогою alert вивести "Hello, John! How are you?" , 
+де John це те, що ввів користувач */
+
+// 1 вариант
+// const userName = prompt('Put your name, please', 'John');
+// alert(`Hello, ${userName}! How are you?`)
+
+// 2 вариант
+// const UserName1 = prompt('Put your name, please', '');
+// alert("Hello, " + UserName1 + "! " + "How are you?" )
+
+/*  HOME TASK 3
+В якості ДЗ, необхідно вирішити дві наступні задачі:
+
+Отримати від користувача 3 рядки та вивести їх у 
+довільному порядку однією командою (конкатенація);
+
+*/
+
+// let str1 = prompt('Put please your first string part', 'xxx');
+// let str2 = prompt('Put please your second string part', 'yyy');
+// let str3 = prompt('Put please your third  string part', 'zzz');
+
+// checking type of date for compilling:
+
+// console.log(typeof str1);  //return STRING
+// console.log(typeof str2);  //return STRING
+// console.log(typeof str3);  //return STRING
+
+// console.log( str1 + str2 + str3);
+// console.log(`In order 213: ${str2}${str1}${str3}`);
+
+/*Розбити за цифрами п'ятизначне число і вивести у вихідному 
+порядку через пробіл.*/
+
+var i = 45678;
+var num = i.toString();
+// 1 вариант
+var a = num[0];
+var b = num[1];
+var c = num[2];
+var d = num[3];
+var e = num[4];
+console.log(a + ' ' + b + ' ' + c + ' ' + d + ' ' + e);
+// 2 вариант
+var num1 = num.split('').join(' ');
+console.log(num1);
+
+/*  HOME TASK 4
+Створити скрипт для додавання, віднімання, множення та поділу 
+двох чисел та виведення результатів.
+
+Користувач вводить два числа через два промпти, 
+і повідомлення виводиться алертом з таким результатом:
+
+//Користувач ввів 2 і 2:
+*/
+var firstNum = prompt('Put first number', '');
+var secNum = prompt('Put second number', '');
+
+// // ' for  Division put"/",  for Multiplication put "+", for Addition put "+", for Subtraction put "-"
+
+var ab = Number(firstNum);
+console.log(_typeof(ab)); // I am checking type of date *string to Number*
+var cd = Number(secNum);
+alert("User put frst number ".concat(ab, " and the second number ").concat(cd));
+var divNum = ab / cd;
+console.log(divNum);
+alert("Division equal: ".concat(ab, " / ").concat(cd, " = ").concat(divNum));
+var multiNum = ab * cd;
+console.log(multiNum);
+alert("Multiplication equal: ".concat(ab, " * ").concat(cd, " = ").concat(multiNum));
+var addNum = ab + cd;
+console.log(addNum);
+alert("Addition equal: ".concat(ab, " + ").concat(cd, " = ").concat(addNum));
+var subtrNum = ab - cd;
+console.log(subtrNum);
+alert("Subtraction equal: ".concat(ab, " - ").concat(cd, " = ").concat(subtrNum));
--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,91 @@
+/* 
+HOME TASK 2
 
-let num = 123456;
-let firstNum = Number(num.toString(10)[4]);
+Tреба реалізувати просту програму з наступними умовами:
 
-console.log(firstNum);
+За допомогою prompt запитати ім'я користувача.
+За допомогою alert вивести "Hello, John! How are you?" , 
+де John це те, що ввів користувач */
 
-if( firstNum == 1 || firstNum == 2 || firstNum == 3) {
-  console.log('yes')
-} else {
-  console.log('no')
-}
+// 1 вариант
+// const userName = prompt('Put your name, please', 'John');
+// alert(`Hello, ${userName}! How are you?`)
 
+// 2 вариант
+// const UserName1 = prompt('Put your name, please', '');
+// alert("Hello, " + UserName1 + "! " + "How are you?" )
+
+
+/*  HOME TASK 3
+В якості ДЗ, необхідно вирішити дві наступні задачі:
+
+Отримати від користувача 3 рядки та вивести їх у 
+довільному порядку однією командою (конкатенація);
+
+*/
+
+// let str1 = prompt('Put please your first string part', 'xxx');
+// let str2 = prompt('Put please your second string part', 'yyy');
+// let str3 = prompt('Put please your third  string part', 'zzz');
+
+// checking type of date for compilling:
+
+// console.log(typeof str1);  //return STRING
+// console.log(typeof str2);  //return STRING
+// console.log(typeof str3);  //return STRING
+
+// console.log( str1 + str2 + str3);
+// console.log(`In order 213: ${str2}${str1}${str3}`);
+
+/*Розбити за цифрами п'ятизначне число і вивести у вихідному 
+порядку через пробіл.*/
+
+let i = 45678;
+let num = i.toString();
+// 1 вариант
+let a = num[0];
+let b = num[1];
+let c = num[2];
+let d = num[3];
+let e = num[4];
+console.log(a + ' ' + b + ' ' + c + ' ' + d + ' ' + e);
+// 2 вариант
+let num1 = num.split('').join(' ');
+console.log(num1);
+
+
+/*  HOME TASK 4
+Створити скрипт для додавання, віднімання, множення та поділу 
+двох чисел та виведення результатів.
+
+Користувач вводить два числа через два промпти, 
+і повідомлення виводиться алертом з таким результатом:
+
+//Користувач ввів 2 і 2:
+*/
+let firstNum = prompt('Put first number', '');
+let secNum = prompt('Put second number', '');
+
+// // ' for  Division put"/",  for Multiplication put "+", for Addition put "+", for Subtraction put "-"
+
+let ab = Number(firstNum);
+console.log(typeof ab); // I am checking type of date *string to Number*
+let cd = Number(secNum);
+alert (`User put frst number ${ab} and the second number ${cd}`);
+
+let divNum = ab / cd;
+console.log(divNum);
+alert(`Division equal: ${ab} / ${cd} = ${divNum}`);
+
+let multiNum = ab * cd;
+console.log(multiNum);
+alert(`Multiplication equal: ${ab} * ${cd} = ${multiNum}`);
+
+
+let addNum = ab + cd;
+console.log(addNum);
+alert( `Addition equal: ${ab} + ${cd} = ${addNum}`);
+
+let subtrNum = ab - cd;
+console.log(subtrNum);
+alert( `Subtraction equal: ${ab} - ${cd} = ${subtrNum}`);

fcaaf352283c3ae42ce4e759a373e575d433cb99
Yuriy Melnykov <y.melnykov@outlook.com>
2023-10-26 09:47:52 +0200

    all need homeworks has done stert add

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,65 +1,56 @@
 "use strict";
 
-// console.log( typeof {} );
-// (10).toString()
+function _typeof(o) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (o) { return typeof o; } : function (o) { return o && "function" == typeof Symbol && o.constructor === Symbol && o !== Symbol.prototype ? "symbol" : typeof o; }, _typeof(o); }
+/*  HOMEWOKR 7
 
-// console.log(null);
+Створити скрипт який має визначити середнє арифметичне 
+трьох чисел з насутпними умовами:
+отримати від користувача через три prompt три числа
+показати через alert середнє арифметичне цих чисел
 
-//  HOMEWOKR 7
-// Створити скрипт який має визначити середнє арифметичне 
-// трьох чисел з насутпними умовами:
-// отримати від користувача через три prompt три числа
-// показати через alert середнє арифметичне цих чисел
+                Variant 1  - simple;               */
 
-// //  Variant 1  - simple;
-// const firstNum1 = +prompt('put your forst number, please', '10');
-// const secondNum1 = +prompt('Put your second number, please', '20');
-// const thirdNum1 = +prompt('Put you third number, please', '40');
-// const quantityNumber1 = 3;
-// const arithmeticMean1 = (firstNum1 + secondNum1 + thirdNum1) / quantityNumber1;
-// alert(`Your arithmeticMean is equal ${arithmeticMean1}`);
+var firstNum1 = +prompt('put your forst number, please', '10');
+var secondNum1 = +prompt('Put your second number, please', '20');
+var thirdNum1 = +prompt('Put you third number, please', '40');
+var quantityNumbers1 = 3;
+var arithmeticMean1 = (firstNum1 + secondNum1 + thirdNum1) / quantityNumbers1;
+alert("Your arithmeticMean is equal ".concat(arithmeticMean1));
 
 // // Variant 2 advanced; (Это так, для себя для тренировки)
-// const firstNum2 = +prompt('Enter your forst number, please', '20.9');
 
-// if(isNaN(firstNum2)) {
-//   alert('Please put a NUMBER', 'for exaple 12.5');
-// }
-
-// const secondNum2 = +prompt('Put your second number, please', '20.4');
-// if(isNaN(secondNum2)) {
-//   alert('Please put a NUMBER', 'for exaple 12.5');
-// }
-// const thirdNum2 = +prompt('Put you third number, please', '45');
-
-// if(isNaN(thirdNum2)) {
-//   alert('Please put a NUMBER', 'for exaple 12.5');
-// }
-// const arithmeticMean2 = (firstNum2 + secondNum2 + thirdNum2) / quantityNumber1;
-// const decimalQuantity = +prompt('What level of precision do you require for rounding?', '0 - 3');
-// let arithmeticMean;
-
-// if (decimalQuantity === 0) {
-//   // toFixed возвращает СТРОКУ!!!
-//   arithmeticMean = +arithmeticMean2.toFixed(0); // не явное преобразование строки в number.
-//   console.log(typeof arithmeticMean, arithmeticMean);
-//   alert(`Your arithmeticMean is equal ${arithmeticMean}`);
-
-// } else if(decimalQuantity === 1 ) {
-//   arithmeticMean = +arithmeticMean2.toFixed(1);
-//   alert(`Your arithmeticMean is equal ${arithmeticMean}`);
-
-// } else if(decimalQuantity === 2 ) {
-//   arithmeticMean = +arithmeticMean2.toFixed(2);
-//   alert(`Your arithmeticMean is equal ${arithmeticMean}`);
-
-// } else if(decimalQuantity === 3 ) {
-//   arithmeticMean = +arithmeticMean2.toFixed(3);
-//   alert(`Your arithmeticMean is equal ${arithmeticMean}`);
-
-// } else {
-//   alert('Please put correct level of precision', '0 -3');
-// }
+var firstNum2 = +prompt('Enter your forst number, please', '20.9');
+if (isNaN(firstNum2)) {
+  alert('Please put a NUMBER', 'for exaple 12.5');
+}
+var secondNum2 = +prompt('Put your second number, please', '20.4');
+if (isNaN(secondNum2)) {
+  alert('Please put a NUMBER', 'for exaple 12.5');
+}
+var thirdNum2 = +prompt('Put you third number, please', '45');
+if (isNaN(thirdNum2)) {
+  alert('Please put a NUMBER', 'for exaple 12.5');
+}
+var arithmeticMean2 = (firstNum2 + secondNum2 + thirdNum2) / quantityNumbers1;
+var decimalQuantity = +prompt('What level of precision do you require for rounding?', '0 - 3');
+var arithmeticMean;
+if (decimalQuantity === 0) {
+  // toFixed возвращает СТРОКУ!!!
+  arithmeticMean = +arithmeticMean2.toFixed(0); // не явное преобразование строки в number.
+  console.log(_typeof(arithmeticMean), arithmeticMean);
+  alert("Your arithmeticMean is equal ".concat(arithmeticMean));
+} else if (decimalQuantity === 1) {
+  arithmeticMean = +arithmeticMean2.toFixed(1);
+  alert("Your arithmeticMean is equal ".concat(arithmeticMean));
+} else if (decimalQuantity === 2) {
+  arithmeticMean = +arithmeticMean2.toFixed(2);
+  alert("Your arithmeticMean is equal ".concat(arithmeticMean));
+} else if (decimalQuantity === 3) {
+  arithmeticMean = +arithmeticMean2.toFixed(3);
+  alert("Your arithmeticMean is equal ".concat(arithmeticMean));
+} else {
+  alert('Please put correct level of precision', '0 -3');
+}
 
 /*  HOMEWORK 8 (two parts!!!)
 
@@ -89,61 +80,51 @@
 
 //  Частина №1 i 2
 
-// let userYearOld;
-// let userTown;
-// let userSport;
-// const currentYear = 2023;
-// const userBithText = 'year of your Birthday';
-// const userResidenceText = 'your town where do you live';
-// const userFavSportText = 'you favorite sport';
-// const townIndicate = 'you live in';
-// const capitIndicate = `${townIndicate} the capital of`;
-// const userAwesome = 'AWESOME! Do you want to become like ';
-
-// const userAgree = confirm('Would you like to answer our questions about your age, place of residence, and favorite sport?','');
-// if (userAgree === true) {
-// console.log(userAgree);
-
-//   const userBirthYear = +prompt(`Enter, please, year of your birthday`, 'yyyy / 1995');
-//   userYearOld = currentYear - userBirthYear;
-
-//   const userTownAnswer = prompt('Enter the name of the town where do you live', ' Lion');
-//   if(userTownAnswer === 'London') {
-//     userTown = capitIndicate + ' England';
-//   } else if(userTownAnswer === 'Kiev') {
-//     userTown = capitIndicate + ' Ukraine';
-//   } else if(userTownAnswer === 'Washington') {
-//     userTown = capitIndicate + ' USA';
-//   } else {
-//     userTown = townIndicate + userTownAnswer;
-//   }
-
-//   const userFavSport = prompt('What is your favorite sport?','sprint / tennis / swimming');
-//   if(userFavSport === 'sprint') {
-//     userSport = `${userAwesome} Usain Bolt?`;
-//   } else if (userFavSport === 'tennis') {
-//     userSport = `${userAwesome} Serena Williams?`;
-//   } else if(userFavSport === 'swimming') {
-//     userSport = `${userAwesome} Michael Phelps?`;
-//   } 
-//   else {
-//     userSport = `Your favorite sport is ${userFavSport}`
-//   }
-
-//   alert(`You have ${userYearOld} years old,
-//   and ${userTown}
-//   ${userSport} `);
-
-// } else if(userAgree === false) {
-//   alert(`I'm sorry that you don't want to tell us about 
-//   ${userBithText} 
-//   ${userResidenceText} 
-//   and ${userFavSportText}`);
-// }
+var userYearOld;
+var userTown;
+var userSport;
+var currentYear = 2023;
+var userBithText = 'year of your Birthday';
+var userResidenceText = 'your town where do you live';
+var userFavSportText = 'you favorite sport';
+var townIndicate = 'you live in';
+var capitIndicate = "".concat(townIndicate, " the capital of");
+var userAwesome = 'AWESOME! Do you want to become like ';
+var userAgree = confirm('Would you like to answer our questions about your age, place of residence, and favorite sport?', '');
+if (userAgree === true) {
+  console.log(userAgree);
+  var userBirthYear = +prompt("Enter, please, year of your birthday", 'yyyy / 1995');
+  userYearOld = currentYear - userBirthYear;
+  var userTownAnswer = prompt('Enter the name of the town where do you live', ' Lion');
+  if (userTownAnswer === 'London') {
+    userTown = capitIndicate + ' England';
+  } else if (userTownAnswer === 'Kiev') {
+    userTown = capitIndicate + ' Ukraine';
+  } else if (userTownAnswer === 'Washington') {
+    userTown = capitIndicate + ' USA';
+  } else {
+    userTown = townIndicate + userTownAnswer;
+  }
+  var userFavSport = prompt('What is your favorite sport?', 'sprint / tennis / swimming');
+  if (userFavSport === 'sprint') {
+    userSport = "".concat(userAwesome, " Usain Bolt?");
+  } else if (userFavSport === 'tennis') {
+    userSport = "".concat(userAwesome, " Serena Williams?");
+  } else if (userFavSport === 'swimming') {
+    userSport = "".concat(userAwesome, " Michael Phelps?");
+  } else {
+    userSport = "Your favorite sport is ".concat(userFavSport);
+  }
+  alert("You have ".concat(userYearOld, " years old,\n  and ").concat(userTown, "\n  ").concat(userSport, " "));
+} else if (userAgree === false) {
+  alert("I'm sorry that you don't want to tell us about \n  ".concat(userBithText, " \n  ").concat(userResidenceText, " \n  and ").concat(userFavSportText));
+}
 
 /*  HOMEWORK 9 
 Переписати код нижче з використанням конструкції switch…case*/
-/*
+
+/*   ИСХОДНЫЙ КОД:
+
 let numOrStr = prompt('input number or string');
 console.log(numOrStr)
 
@@ -155,7 +136,26 @@ console.log('Empty String');
 console.log(' number is Ba_NaN')
 } else {
 console.log('OK!')
-}*/
+}
+
+        РЕШЕНИЕ ДОМАШНЕГО ЗАДАНИЯ С ИСПОЛЬЗОВАНИЕМ switch     */
+
+var numOrStr = prompt('input number or string');
+console.log(numOrStr);
+switch (numOrStr) {
+  case null:
+    console.log('ви скасували');
+    brake;
+  case '':
+    console.log('Empty String');
+    brake;
+  case NaN:
+    console.log(' number is Ba_NaN');
+    brake;
+  default:
+    console.log('OK!');
+    brake;
+}
 
 // Hometask "Check - list"
 //FALSE
@@ -182,9 +182,3 @@ console.log(Boolean('NaN'));       // true
 console.log(Boolean('null'));      // true
 console.log(Boolean('undefined')); // true
 */
-var test = true;
-if (!test) {
-  console.log('+++++');
-} else {
-  console.log('----');
-}
--- src/js/script.js
+++ src/js/script.js
@@ -1,63 +1,63 @@
-// console.log( typeof {} );
-// (10).toString()
 
-// console.log(null);
 
-//  HOMEWOKR 7
-// Створити скрипт який має визначити середнє арифметичне 
-// трьох чисел з насутпними умовами:
-// отримати від користувача через три prompt три числа
-// показати через alert середнє арифметичне цих чисел
+/*  HOMEWOKR 7
 
-// //  Variant 1  - simple;
-// const firstNum1 = +prompt('put your forst number, please', '10');
-// const secondNum1 = +prompt('Put your second number, please', '20');
-// const thirdNum1 = +prompt('Put you third number, please', '40');
-// const quantityNumber1 = 3;
-// const arithmeticMean1 = (firstNum1 + secondNum1 + thirdNum1) / quantityNumber1;
-// alert(`Your arithmeticMean is equal ${arithmeticMean1}`);
+Створити скрипт який має визначити середнє арифметичне 
+трьох чисел з насутпними умовами:
+отримати від користувача через три prompt три числа
+показати через alert середнє арифметичне цих чисел
+
+                Variant 1  - simple;               */
+
+const firstNum1 = +prompt('put your forst number, please', '10');
+const secondNum1 = +prompt('Put your second number, please', '20');
+const thirdNum1 = +prompt('Put you third number, please', '40');
+const quantityNumbers1 = 3;
+const arithmeticMean1 = (firstNum1 + secondNum1 + thirdNum1) / quantityNumbers1;
+alert(`Your arithmeticMean is equal ${arithmeticMean1}`);
 
 // // Variant 2 advanced; (Это так, для себя для тренировки)
-// const firstNum2 = +prompt('Enter your forst number, please', '20.9');
 
-// if(isNaN(firstNum2)) {
-//   alert('Please put a NUMBER', 'for exaple 12.5');
-// }
+const firstNum2 = +prompt('Enter your forst number, please', '20.9');
 
-// const secondNum2 = +prompt('Put your second number, please', '20.4');
-// if(isNaN(secondNum2)) {
-//   alert('Please put a NUMBER', 'for exaple 12.5');
-// }
-// const thirdNum2 = +prompt('Put you third number, please', '45');
+if(isNaN(firstNum2)) {
+  alert('Please put a NUMBER', 'for exaple 12.5');
+}
 
-// if(isNaN(thirdNum2)) {
-//   alert('Please put a NUMBER', 'for exaple 12.5');
-// }
-// const arithmeticMean2 = (firstNum2 + secondNum2 + thirdNum2) / quantityNumber1;
-// const decimalQuantity = +prompt('What level of precision do you require for rounding?', '0 - 3');
-// let arithmeticMean;
+const secondNum2 = +prompt('Put your second number, please', '20.4');
+if(isNaN(secondNum2)) {
+  alert('Please put a NUMBER', 'for exaple 12.5');
+}
+const thirdNum2 = +prompt('Put you third number, please', '45');
 
-// if (decimalQuantity === 0) {
-//   // toFixed возвращает СТРОКУ!!!
-//   arithmeticMean = +arithmeticMean2.toFixed(0); // не явное преобразование строки в number.
-//   console.log(typeof arithmeticMean, arithmeticMean);
-//   alert(`Your arithmeticMean is equal ${arithmeticMean}`);
+if(isNaN(thirdNum2)) {
+  alert('Please put a NUMBER', 'for exaple 12.5');
+}
+const arithmeticMean2 = (firstNum2 + secondNum2 + thirdNum2) / quantityNumbers1;
+const decimalQuantity = +prompt('What level of precision do you require for rounding?', '0 - 3');
+let arithmeticMean;
 
-// } else if(decimalQuantity === 1 ) {
-//   arithmeticMean = +arithmeticMean2.toFixed(1);
-//   alert(`Your arithmeticMean is equal ${arithmeticMean}`);
+if (decimalQuantity === 0) {
+  // toFixed возвращает СТРОКУ!!!
+  arithmeticMean = +arithmeticMean2.toFixed(0); // не явное преобразование строки в number.
+  console.log(typeof arithmeticMean, arithmeticMean);
+  alert(`Your arithmeticMean is equal ${arithmeticMean}`);
 
-// } else if(decimalQuantity === 2 ) {
-//   arithmeticMean = +arithmeticMean2.toFixed(2);
-//   alert(`Your arithmeticMean is equal ${arithmeticMean}`);
+} else if(decimalQuantity === 1 ) {
+  arithmeticMean = +arithmeticMean2.toFixed(1);
+  alert(`Your arithmeticMean is equal ${arithmeticMean}`);
 
-// } else if(decimalQuantity === 3 ) {
-//   arithmeticMean = +arithmeticMean2.toFixed(3);
-//   alert(`Your arithmeticMean is equal ${arithmeticMean}`);
+} else if(decimalQuantity === 2 ) {
+  arithmeticMean = +arithmeticMean2.toFixed(2);
+  alert(`Your arithmeticMean is equal ${arithmeticMean}`);
 
-// } else {
-//   alert('Please put correct level of precision', '0 -3');
-// }
+} else if(decimalQuantity === 3 ) {
+  arithmeticMean = +arithmeticMean2.toFixed(3);
+  alert(`Your arithmeticMean is equal ${arithmeticMean}`);
+
+} else {
+  alert('Please put correct level of precision', '0 -3');
+}
 
 /*  HOMEWORK 8 (two parts!!!)
 
@@ -89,62 +89,64 @@
 //  Частина №1 i 2
 
 
-// let userYearOld;
-// let userTown;
-// let userSport;
-// const currentYear = 2023;
-// const userBithText = 'year of your Birthday';
-// const userResidenceText = 'your town where do you live';
-// const userFavSportText = 'you favorite sport';
-// const townIndicate = 'you live in';
-// const capitIndicate = `${townIndicate} the capital of`;
-// const userAwesome = 'AWESOME! Do you want to become like ';
+let userYearOld;
+let userTown;
+let userSport;
+const currentYear = 2023;
+const userBithText = 'year of your Birthday';
+const userResidenceText = 'your town where do you live';
+const userFavSportText = 'you favorite sport';
+const townIndicate = 'you live in';
+const capitIndicate = `${townIndicate} the capital of`;
+const userAwesome = 'AWESOME! Do you want to become like ';
 
-// const userAgree = confirm('Would you like to answer our questions about your age, place of residence, and favorite sport?','');
-// if (userAgree === true) {
-// console.log(userAgree);
+const userAgree = confirm('Would you like to answer our questions about your age, place of residence, and favorite sport?','');
+if (userAgree === true) {
+console.log(userAgree);
 
-//   const userBirthYear = +prompt(`Enter, please, year of your birthday`, 'yyyy / 1995');
-//   userYearOld = currentYear - userBirthYear;
+  const userBirthYear = +prompt(`Enter, please, year of your birthday`, 'yyyy / 1995');
+  userYearOld = currentYear - userBirthYear;
   
-//   const userTownAnswer = prompt('Enter the name of the town where do you live', ' Lion');
-//   if(userTownAnswer === 'London') {
-//     userTown = capitIndicate + ' England';
-//   } else if(userTownAnswer === 'Kiev') {
-//     userTown = capitIndicate + ' Ukraine';
-//   } else if(userTownAnswer === 'Washington') {
-//     userTown = capitIndicate + ' USA';
-//   } else {
-//     userTown = townIndicate + userTownAnswer;
-//   }
+  const userTownAnswer = prompt('Enter the name of the town where do you live', ' Lion');
+  if(userTownAnswer === 'London') {
+    userTown = capitIndicate + ' England';
+  } else if(userTownAnswer === 'Kiev') {
+    userTown = capitIndicate + ' Ukraine';
+  } else if(userTownAnswer === 'Washington') {
+    userTown = capitIndicate + ' USA';
+  } else {
+    userTown = townIndicate + userTownAnswer;
+  }
 
-//   const userFavSport = prompt('What is your favorite sport?','sprint / tennis / swimming');
-//   if(userFavSport === 'sprint') {
-//     userSport = `${userAwesome} Usain Bolt?`;
-//   } else if (userFavSport === 'tennis') {
-//     userSport = `${userAwesome} Serena Williams?`;
-//   } else if(userFavSport === 'swimming') {
-//     userSport = `${userAwesome} Michael Phelps?`;
-//   } 
-//   else {
-//     userSport = `Your favorite sport is ${userFavSport}`
-//   }
+  const userFavSport = prompt('What is your favorite sport?','sprint / tennis / swimming');
+  if(userFavSport === 'sprint') {
+    userSport = `${userAwesome} Usain Bolt?`;
+  } else if (userFavSport === 'tennis') {
+    userSport = `${userAwesome} Serena Williams?`;
+  } else if(userFavSport === 'swimming') {
+    userSport = `${userAwesome} Michael Phelps?`;
+  } 
+  else {
+    userSport = `Your favorite sport is ${userFavSport}`
+  }
 
-//   alert(`You have ${userYearOld} years old,
-//   and ${userTown}
-//   ${userSport} `);
+  alert(`You have ${userYearOld} years old,
+  and ${userTown}
+  ${userSport} `);
 
-// } else if(userAgree === false) {
-//   alert(`I'm sorry that you don't want to tell us about 
-//   ${userBithText} 
-//   ${userResidenceText} 
-//   and ${userFavSportText}`);
-// }
+} else if(userAgree === false) {
+  alert(`I'm sorry that you don't want to tell us about 
+  ${userBithText} 
+  ${userResidenceText} 
+  and ${userFavSportText}`);
+}
 
 
 /*  HOMEWORK 9 
 Переписати код нижче з використанням конструкції switch…case*/
-/*
+
+/*   ИСХОДНЫЙ КОД:
+
 let numOrStr = prompt('input number or string');
 console.log(numOrStr)
 
@@ -156,7 +158,28 @@ console.log('Empty String');
 console.log(' number is Ba_NaN')
 } else {
 console.log('OK!')
-}*/
+}
+
+        РЕШЕНИЕ ДОМАШНЕГО ЗАДАНИЯ С ИСПОЛЬЗОВАНИЕМ switch     */
+
+let numOrStr = prompt('input number or string');
+console.log(numOrStr)
+
+switch(numOrStr) {
+  case null:
+    console.log('ви скасували');
+  brake;
+  case '':
+    console.log('Empty String');
+  brake;
+  case NaN:
+    console.log(' number is Ba_NaN');
+  brake;
+  default:
+    console.log('OK!');
+  brake;
+}
+
 
 
 
@@ -185,10 +208,3 @@ console.log(Boolean('NaN'));       // true
 console.log(Boolean('null'));      // true
 console.log(Boolean('undefined')); // true
 */
-let test = true;
-
-if (!test) {
-  console.log('+++++');
-} else {
-  console.log('----');
-}

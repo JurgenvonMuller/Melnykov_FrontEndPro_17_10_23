ee8db25a28ec90ca1bb9351484175f86042903ba
Yuriy Melnykov <y.melnykov@outlook.com>
2023-10-26 11:37:51 +0200

    all

--- dist/js/script.js
+++ dist/js/script.js
@@ -138,7 +138,7 @@ console.log(' number is Ba_NaN')
 console.log('OK!')
 }
 
-        РЕШЕНИЕ ДОМАШНЕГО ЗАДАНИЯ С ИСПОЛЬЗОВАНИЕМ switch     */
+        РЕШЕНИЕ ДОМАШНЕГО ЗАДАНИЯ С ИСПОЛЬЗОВАНИЕМ switch / case    */
 
 var numOrStr = prompt('input number or string');
 console.log(numOrStr);
@@ -157,7 +157,7 @@ switch (numOrStr) {
     brake;
 }
 
-/*      Hometask "Check - list"
+/*      Hometask additional "Check - list"
         
        FALSE
 console.log(Boolean(0));         // false
@@ -183,3 +183,133 @@ console.log(Boolean('NaN'));       // true
 console.log(Boolean('null'));      // true
 console.log(Boolean('undefined')); // true
 */
+
+/*
+  1 const/let
+  2 variable names
+  3 correct output
+*/
+
+// HOME TASK - TO READ BY YOURSELF
+/*number.toString()
+
+10.toString();
+
+JS воспринмимает эту запись как попытку обратьтся к числу с плавающей точкой
+то есть к десятичной дроби. 
+Что бы избежать этого надо использовать один из методов изменения записи ЦЕЛЫХ чисел перед 
+toString() 
+
+var number = 10;
+number.toString(); или
+10..toString();     или
+10 .toString();    или
+(10).toString();   
+
+В то же время проблем с действительными дробными числами не возникает и 
+запись может быть такова: 
+
+12.5.toString(); // 12.5 преобразовывается в строку без проблем.
+
+
+
+
+// javascript number literal to string problem 
+
+/*
+number
+number -> string
+number -> boolean (0, NaN -> false / ALL -> true)
+
+// const number = 10;
+// const string1 = number.toString(); // вернет '10'
+// const string2 = String(number);    // вернет '10'
+// const string3 = number + '';       // вернет '10'
+// const string4 = `${number}`;       // вернет '10'
+// const boolean = Boolean(number);   // // вернет true (Boolean)
+*/
+/*
+string
+string -> number
+string -> boolean ('' -> false / all other -> true)
+
+// const string = '10';
+// const number1 = Number(string);  // вернет 10 как число.
+// const number2 = +string;         // вернет 10 как число.
+// const number3 = string / 5;      // вернет 2 как число
+// const boolean = Boolean(string); //вернет true как булево значение
+*/
+/*
+const a;
+String(a);
+Number(a);
+Boolean(a);
+
+undefined -> string -> 'undefined' 
+undefined -> number -> NaN
+undefined -> boolean -> false
+*/
+/*
+null -> string -> 'null'
+null -> number -> 0
+null -> boolean -> false
+*/
+/*
+boolean -> string -> 'true'/'false'
+boolean -> number -> 1 / 0
+*/
+// HOMETASK 2 - > create a table with all transformations
+// ------ 
+// WTF JS  ---------------  просмотрено но не осознано )))
+
+// const a = 10;
+// const b = '10';
+// const c = true;
+
+var check1 = '1' == boolean;
+var check2 = check1 == Boolean(check1); // true
+
+// const checkToNumber1 = a === Number(b);  // false
+// const checkToString1 = String(a) === b;  // true
+// const checkToNumber2 = a !== Number(b);  // false
+// const checkToString2 = String(a) !== b;  // false
+// const checkToNumber3 = a != Number(b);   // false
+// const checkToString3 = String(a) != b;   // false
+
+// a === b => true  ???? //false так как ф - число а b - строка!!!!
+// 1 !== 1 => false
+// 1 === 2 => false
+// 1 !== 2 => true
+
+// if (check1) {
+//   alert('YES, THEY ARE EQUAL!');
+// }
+
+// if (check2) {
+//   // DO THIS
+// }
+
+// const a = 10;
+// const b = '10';
+// const equal = a === b; // false
+// // const notEqual = a !== b; // true
+// const notEqual1 = !a; // false
+// const notEqual2 = !b; // false
+// const notEqual3 = !equal; // true
+
+// const a = !!alert(1); // false
+// const b = !!10; // true
+// const c = !!'10'; // true
+// const d = !!''; // false
+
+// Boolean(myVariable);
+// const a = !myVar; // true/false
+// const b = !a; // false/true
+// const c = +b; // 0/1
+// const d = (+(!(!myVar)));
+
+// const string = 'a';
+// const number = Number(string);
+
+// const a = true || ...; // -> true
+// const c = false && ...; // -> false

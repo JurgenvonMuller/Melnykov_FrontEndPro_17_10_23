98640bdf4f02a225e29b1ba4e19339919d3d704e
Yuriy Melnykov <y.melnykov@outlook.com>
2023-10-30 13:26:01 +0100

    the end)

--- dist/js/script.js
+++ dist/js/script.js
@@ -4,87 +4,88 @@
                   HOMEWORK 10 
 Вивести на сторінку в один рядок через кому числа від 10 до 20.
 */
-// for( let num = 10; num <=20; num++) {
-//   document.write(` ${num}`); // вывод на страницу сайта как в задании НЕ В КОНСОЛЬ!
-// }
+for (var num = 10; num <= 20; num++) {
+  document.write(" ".concat(num)); // вывод на страницу сайта как в задании НЕ В КОНСОЛЬ!
+}
 
 /*
 Вивести квадрати чисел від 10 до 20.
 */
 
-// for(let num1 = 10; num1 <= 20; num1++) {
-//   let exp = 2;
-//   let res1 = num1 ** exp;
-//   //let res1 = num1 **2;  ----- , без доп переменной exp.
-//   document.write('  ' + res1 + '  ');
-// }
-// document.write(`<br>`);
+for (var num1 = 10; num1 <= 20; num1++) {
+  var exp = 2;
+  var res1 = Math.pow(num1, exp);
+  //let res1 = num1 **2;  ----- , без доп переменной exp.
+  document.write('  ' + res1 + '  ');
+}
+document.write("<br>");
 
 /*
 Вивести таблицю множення на 7.
 */
 
-// let num2 = 7;
-// for( let multip = 0; multip <= 10; multip++) {
-//   let result2 = num2 * multip;
-//   document.write(`
-//   ${num2} * ${multip} = ${result2} <br>`);
-// }
-// document.write(`<br>`);
+var num2 = 7;
+for (var multip = 0; multip <= 10; multip++) {
+  var result2 = num2 * multip;
+  document.write("\n  ".concat(num2, " * ").concat(multip, " = ").concat(result2, " <br>"));
+}
+document.write("<br>");
 
 /*
 Знайти суму всіх цілих чисел від 1 до 15.
 */
 
-// let result3 = 0;
-// for(let num3 = 1; num3 <=15; num3++) {
-//   result3 += num3;
-// }
-// document.write(result3);
-// document.write(`<br>`);
+var result3 = 0;
+for (var num3 = 1; num3 <= 15; num3++) {
+  result3 += num3;
+}
+document.write(result3);
+document.write("<br>");
 
 /*
 Знайти добуток усіх цілих чисел від 15 до 35.
 */
 
-// let result4 = 1;
-// for(let num4 = 15; num4 <= 35; num4++) {
-//   result4 *= num4;
-// }
-// document.write(result4);
-// document.write(`<br>`);
+var result4 = 1;
+for (var num4 = 15; num4 <= 35; num4++) {
+  result4 *= num4;
+}
+document.write(result4);
+document.write("<br>");
 
 /*
 Знайти середнє арифметичне всіх цілих чисел від 1 до 500.
 */
-// let count = 0; 
-// let summ = 0;
-// for( let num5 = 1; num5 <= 500; num5++) {
-//   summ += num5;
-//   count++
-//   // console.log(summ, count);
-// }
-// console.log( summ / count);
+var count = 0;
+var summ = 0;
+for (var num5 = 1; num5 <= 500; num5++) {
+  summ += num5;
+  count++;
+  // console.log(summ, count);
+}
+
+console.log(summ / count); // тут можно бы было ввеси еще одну переменную но зачем?
 
 /* 
 Вивести суму лише парних чисел в діапазоні від 30 до 80.
 */
 
-// for(let num6 = 30; num6 <= 80; num6++) {
-//   if(num6 % 2 === 0) {
-//     console.log (num6);
-//   }
-// }
+for (var num6 = 30; num6 <= 80; num6++) {
+  // как вариант  num6 = num6 + 2
+  if (num6 % 2 === 0) {
+    console.log(num6);
+  }
+}
 
 /*
 Вивести всі числа в діапазоні від 100 до 200 кратні 3.
 */
 
-// for (let num7 = 100; num7 <=200; num7++) {
-//   if(num7 % 3 === 0 )  {
-//     console.log(num7);
-//   }
-// }
+for (var num7 = 100; num7 <= 200; num7++) {
+  if (num7 % 3 === 0) {
+    console.log(num7);
+  }
+}
 
 /*
 Дано натуральне число. Знайти та вивести на сторінку всі його дільники.
@@ -92,19 +93,18 @@
 Знайти суму його парних дільників.
 */
 
-// let naturNumber = +prompt("Slava, enter here any natural number, please", '');
-// let count1 = 0;
-// let sumOfDivis = 0;
-
-// for (let divis = 1; divis <= naturNumber; divis++) {
-//   if(naturNumber % divis === 0) {
-//     console.log(divis);
-//     sumOfDivis += divis;
-//     count1++
-//   }
-// }
-// console.log(`Summ of divisors is ${sumOfDivis}`);
-// console.log(`NUMBER OF DIVISORS OF THIS NATURAL NUMBER IS ${count1}`);
+var naturNumber = +prompt("Slava, enter here any natural number, please", '');
+var count1 = 0;
+var sumOfDivis = 0;
+for (var divis = 1; divis <= naturNumber; divis++) {
+  if (naturNumber % divis === 0) {
+    console.log(divis);
+    sumOfDivis += divis;
+    count1++;
+  }
+}
+console.log("Summ of divisors is ".concat(sumOfDivis));
+console.log("NUMBER OF DIVISORS OF THIS NATURAL NUMBER IS ".concat(count1));
 
 /*
 Надрукувати повну таблицю множення від 1 до 10.
@@ -123,23 +123,23 @@
               ВЛОЖЕННЫЙ ЦИКЛ........ МДЯ....
 */
 
-// for(let firstNumber = 1; firstNumber <=10; firstNumber++ ) {
-//   for(let secondNumber = 1; secondNumber <=10; secondNumber++ ) {
-//     let multResult = firstNumber * secondNumber;
-//     console.log(` ${firstNumber} * ${secondNumber} = ${multResult} `);
-//   }
-// }
+for (var firstNumber = 1; firstNumber <= 10; firstNumber++) {
+  for (var secondNumber = 1; secondNumber <= 10; secondNumber++) {
+    var multResult = firstNumber * secondNumber;
+    console.log(" ".concat(firstNumber, " * ").concat(secondNumber, " = ").concat(multResult, " "));
+  }
+}
 
 /* HOMEWORK 11 
 Написати цикли, які роблять наступне:\
 Вивести числа від 20 до 30 через пропуск, використовуючи крок 0,5 (20 20,5 21 21,5….).
 */
 
-// for( let numberOne = 20; numberOne <= 30; numberOne += 0.5) {
-//   let resultNumber = numberOne;
-//   console.log(resultNumber);
-//   document.write(`  ${resultNumber}, `)
-// }
+for (var numberOne = 20; numberOne <= 30; numberOne += 0.5) {
+  var resultNumber = numberOne;
+  console.log(resultNumber);
+  document.write("  ".concat(resultNumber, ", "));
+}
 
 /*
 Один долар коштує 27 гривень. Вивести дані з розрахунком вартості 10, 20, 30... 100 доларів.
@@ -156,21 +156,20 @@
 квадрат яких не перевищує числа N.
 */
 
-// let numberN = +prompt (`enter you number`, '');
-
-// for (let j = 1; j <= 100; j++) {
-// let result = j ** 2; 
-//   if( result < numberN && result % 1 === 0)  {  
-/*
-( result < numberN && Number.isInteger(result)) - стандартная функция Number 
-можно использовать------------------------------
-                      для проверки является ли число целым 
-и доп информация:
-Math.sqrt (result) - возвращает корень от result. то есть j.
-*/
-//     console.log(result);
-//   }
-// }
+var numberN = +prompt("enter you number", '');
+for (var j = 1; j <= 100; j++) {
+  var result = Math.pow(j, 2);
+  if (result < numberN && result % 1 === 0) {
+    /*
+    ( result < numberN && Number.isInteger(result)) - стандартная функция Number 
+    можно использовать------------------------------
+                          для проверки является ли число целым 
+    и доп информация:
+    Math.sqrt (result) - возвращает корень от result. то есть j.
+    */
+    console.log(result);
+  }
+}
 
 /*
 Дане ціле число. З'ясувати, чи є воно простим (простим називається число, більше 1,
@@ -212,20 +211,25 @@ if (numberM <= 1) {
 (Наприклад, числа 9, 81 можна отримати, а 13 - не можна).
 */
 
-// let userNumberAny = +prompt(`Put your number`, ''); 
-// let logarifmPoOsnowaniu3 = Math.log(userNumberAny) / Math.log(3); //обратная функция от возведения 3 в степень.
-// let isNum3 = Number.isInteger(logarifmPoOsnowaniu3); 
-// // Проверяем, является ли результат целым числом функция возвращает булево ТРУ или ФАЛЬШ
+// ТУТ Я ЧЕСТНО ПРОБОВАЛ ЧЕРЕЗ ЛАГОРИФМЫ )))) 
+// let userNumberAny = +prompt(`Put your number`, '');
+// let checkSqrt = Math.log(userNumberAny) / Math.log(3);
+// console.log(checkSqrt); // возвращает 2.0000000000000004 поэтому округляем??? ) 
+// и ничего не получится ))))) 
 
-// if (isNum3) {
-//     console.log(` ${userNumberAny}  может быть получено путем возведения числа 3 в степень.`);
+// if (userNumberAny > 0 && checkSqrt % 1 === 0) {
+//     console.log(`${userNumberAny} может быть получено путем возведения числа 3 в степень.`);
 // } else {
-//     console.log(` ${userNumberAny}  Не может быть получено путем возведения числа 3 в степень.`);
+//     console.log(`${userNumberAny} Не может быть получено путем возведения числа 3 в степень.`);
 // }
+// способ деревянный но верный )))) Хотя долгий при вводе больших чисел.
 
-var userNumberAny = +prompt("Put your number", '');
-var isPowerOf3 = Math.log(userNumberAny) / Math.log(3);
-if (userNumberAny > 0 && isPowerOf3 % 1 === 0) {
+var userNumberAny = +prompt("\u0412\u0432\u0435\u0434\u0438\u0442\u0435 \u0432\u0430\u0448\u0435 \u0447\u0438\u0441\u043B\u043E", '');
+var x = 1;
+while (x < userNumberAny) {
+  x *= 3;
+}
+if (x === userNumberAny) {
   console.log("".concat(userNumberAny, " \u043C\u043E\u0436\u0435\u0442 \u0431\u044B\u0442\u044C \u043F\u043E\u043B\u0443\u0447\u0435\u043D\u043E \u043F\u0443\u0442\u0435\u043C \u0432\u043E\u0437\u0432\u0435\u0434\u0435\u043D\u0438\u044F \u0447\u0438\u0441\u043B\u0430 3 \u0432 \u0441\u0442\u0435\u043F\u0435\u043D\u044C."));
 } else {
   console.log("".concat(userNumberAny, " \u041D\u0435 \u043C\u043E\u0436\u0435\u0442 \u0431\u044B\u0442\u044C \u043F\u043E\u043B\u0443\u0447\u0435\u043D\u043E \u043F\u0443\u0442\u0435\u043C \u0432\u043E\u0437\u0432\u0435\u0434\u0435\u043D\u0438\u044F \u0447\u0438\u0441\u043B\u0430 3 \u0432 \u0441\u0442\u0435\u043F\u0435\u043D\u044C."));
--- src/js/script.js
+++ src/js/script.js
@@ -2,88 +2,88 @@
                   HOMEWORK 10 
 Вивести на сторінку в один рядок через кому числа від 10 до 20.
 */
-// for( let num = 10; num <=20; num++) {
-//   document.write(` ${num}`); // вывод на страницу сайта как в задании НЕ В КОНСОЛЬ!
-// }
+for( let num = 10; num <=20; num++) {
+  document.write(` ${num}`); // вывод на страницу сайта как в задании НЕ В КОНСОЛЬ!
+}
 
 /*
 Вивести квадрати чисел від 10 до 20.
 */
 
-// for(let num1 = 10; num1 <= 20; num1++) {
-//   let exp = 2;
-//   let res1 = num1 ** exp;
-//   //let res1 = num1 **2;  ----- , без доп переменной exp.
-//   document.write('  ' + res1 + '  ');
-// }
-// document.write(`<br>`);
+for(let num1 = 10; num1 <= 20; num1++) {
+  let exp = 2;
+  let res1 = num1 ** exp;
+  //let res1 = num1 **2;  ----- , без доп переменной exp.
+  document.write('  ' + res1 + '  ');
+}
+document.write(`<br>`);
 
 /*
 Вивести таблицю множення на 7.
 */
 
-// let num2 = 7;
-// for( let multip = 0; multip <= 10; multip++) {
-//   let result2 = num2 * multip;
-//   document.write(`
-//   ${num2} * ${multip} = ${result2} <br>`);
-// }
-// document.write(`<br>`);
+let num2 = 7;
+for( let multip = 0; multip <= 10; multip++) {
+  let result2 = num2 * multip;
+  document.write(`
+  ${num2} * ${multip} = ${result2} <br>`);
+}
+document.write(`<br>`);
 
 /*
 Знайти суму всіх цілих чисел від 1 до 15.
 */
 
-// let result3 = 0;
-// for(let num3 = 1; num3 <=15; num3++) {
-//   result3 += num3;
-// }
-// document.write(result3);
-// document.write(`<br>`);
+let result3 = 0;
+for(let num3 = 1; num3 <=15; num3++) {
+  result3 += num3;
+}
+document.write(result3);
+document.write(`<br>`);
 
 /*
 Знайти добуток усіх цілих чисел від 15 до 35.
 */
 
-// let result4 = 1;
-// for(let num4 = 15; num4 <= 35; num4++) {
-//   result4 *= num4;
-// }
-// document.write(result4);
-// document.write(`<br>`);
+let result4 = 1;
+for(let num4 = 15; num4 <= 35; num4++) {
+  result4 *= num4;
+}
+document.write(result4);
+document.write(`<br>`);
 
 /*
 Знайти середнє арифметичне всіх цілих чисел від 1 до 500.
 */
-// let count = 0; 
-// let summ = 0;
-// for( let num5 = 1; num5 <= 500; num5++) {
-//   summ += num5;
-//   count++
-//   // console.log(summ, count);
-// }
-// console.log( summ / count);
+let count = 0; 
+let summ = 0;
+for( let num5 = 1; num5 <= 500; num5++) {
+  summ += num5;
+  count++
+  // console.log(summ, count);
+}
+console.log( summ / count); // тут можно бы было ввеси еще одну переменную но зачем?
 
 
 /* 
 Вивести суму лише парних чисел в діапазоні від 30 до 80.
 */
 
-// for(let num6 = 30; num6 <= 80; num6++) {
-//   if(num6 % 2 === 0) {
-//     console.log (num6);
-//   }
-// }
+for(let num6 = 30; num6 <= 80; num6++) { // как вариант  num6 = num6 + 2
+  if(num6 % 2 === 0) {
+    console.log (num6);
+  }
+}
 
 /*
 Вивести всі числа в діапазоні від 100 до 200 кратні 3.
 */
 
-// for (let num7 = 100; num7 <=200; num7++) {
-//   if(num7 % 3 === 0 )  {
-//     console.log(num7);
-//   }
-// }
+for (let num7 = 100; num7 <=200; num7++) {
+  if(num7 % 3 === 0 )  {
+    console.log(num7);
+  }
+}
 
 /*
 Дано натуральне число. Знайти та вивести на сторінку всі його дільники.
@@ -91,19 +91,19 @@
 Знайти суму його парних дільників.
 */
 
-// let naturNumber = +prompt("Slava, enter here any natural number, please", '');
-// let count1 = 0;
-// let sumOfDivis = 0;
+let naturNumber = +prompt("Slava, enter here any natural number, please", '');
+let count1 = 0;
+let sumOfDivis = 0;
 
-// for (let divis = 1; divis <= naturNumber; divis++) {
-//   if(naturNumber % divis === 0) {
-//     console.log(divis);
-//     sumOfDivis += divis;
-//     count1++
-//   }
-// }
-// console.log(`Summ of divisors is ${sumOfDivis}`);
-// console.log(`NUMBER OF DIVISORS OF THIS NATURAL NUMBER IS ${count1}`);
+for (let divis = 1; divis <= naturNumber; divis++) {
+  if(naturNumber % divis === 0) {
+    console.log(divis);
+    sumOfDivis += divis;
+    count1++
+  }
+}
+console.log(`Summ of divisors is ${sumOfDivis}`);
+console.log(`NUMBER OF DIVISORS OF THIS NATURAL NUMBER IS ${count1}`);
 
 /*
 Надрукувати повну таблицю множення від 1 до 10.
@@ -122,12 +122,12 @@
               ВЛОЖЕННЫЙ ЦИКЛ........ МДЯ....
 */
 
-// for(let firstNumber = 1; firstNumber <=10; firstNumber++ ) {
-//   for(let secondNumber = 1; secondNumber <=10; secondNumber++ ) {
-//     let multResult = firstNumber * secondNumber;
-//     console.log(` ${firstNumber} * ${secondNumber} = ${multResult} `);
-//   }
-// }
+for(let firstNumber = 1; firstNumber <=10; firstNumber++ ) {
+  for(let secondNumber = 1; secondNumber <=10; secondNumber++ ) {
+    let multResult = firstNumber * secondNumber;
+    console.log(` ${firstNumber} * ${secondNumber} = ${multResult} `);
+  }
+}
 
                                 /* HOMEWORK 11 
 Написати цикли, які роблять наступне:\
@@ -135,11 +135,11 @@
 Вивести числа від 20 до 30 через пропуск, використовуючи крок 0,5 (20 20,5 21 21,5….).
 */
 
-// for( let numberOne = 20; numberOne <= 30; numberOne += 0.5) {
-//   let resultNumber = numberOne;
-//   console.log(resultNumber);
-//   document.write(`  ${resultNumber}, `)
-// }
+for( let numberOne = 20; numberOne <= 30; numberOne += 0.5) {
+  let resultNumber = numberOne;
+  console.log(resultNumber);
+  document.write(`  ${resultNumber}, `)
+}
 
 
 
@@ -158,11 +158,11 @@
 квадрат яких не перевищує числа N.
 */
 
-// let numberN = +prompt (`enter you number`, '');
+let numberN = +prompt (`enter you number`, '');
 
-// for (let j = 1; j <= 100; j++) {
-// let result = j ** 2; 
-//   if( result < numberN && result % 1 === 0)  {  
+for (let j = 1; j <= 100; j++) {
+let result = j ** 2; 
+  if( result < numberN && result % 1 === 0)  {  
 /*
 ( result < numberN && Number.isInteger(result)) - стандартная функция Number 
 можно использовать------------------------------
@@ -170,9 +170,9 @@
 и доп информация:
 Math.sqrt (result) - возвращает корень от result. то есть j.
 */
-//     console.log(result);
-//   }
-// }
+    console.log(result);
+  }
+}
 
 /*
 Дане ціле число. З'ясувати, чи є воно простим (простим називається число, більше 1,
@@ -214,22 +214,27 @@ if(numberM <= 1) {
 */
 
 
-// let userNumberAny = +prompt(`Put your number`, ''); 
-// let logarifmPoOsnowaniu3 = Math.log(userNumberAny) / Math.log(3); //обратная функция от возведения 3 в степень.
-// let isNum3 = Number.isInteger(logarifmPoOsnowaniu3); 
-// // Проверяем, является ли результат целым числом функция возвращает булево ТРУ или ФАЛЬШ
 
-// if (isNum3) {
-//     console.log(` ${userNumberAny}  может быть получено путем возведения числа 3 в степень.`);
+              // ТУТ Я ЧЕСТНО ПРОБОВАЛ ЧЕРЕЗ ЛАГОРИФМЫ )))) 
+// let userNumberAny = +prompt(`Put your number`, '');
+// let checkSqrt = Math.log(userNumberAny) / Math.log(3);
+// console.log(checkSqrt); // возвращает 2.0000000000000004 поэтому округляем??? ) 
+// и ничего не получится ))))) 
+
+// if (userNumberAny > 0 && checkSqrt % 1 === 0) {
+//     console.log(`${userNumberAny} может быть получено путем возведения числа 3 в степень.`);
 // } else {
-//     console.log(` ${userNumberAny}  Не может быть получено путем возведения числа 3 в степень.`);
+//     console.log(`${userNumberAny} Не может быть получено путем возведения числа 3 в степень.`);
 // }
+                // способ деревянный но верный )))) Хотя долгий при вводе больших чисел.
 
+let userNumberAny = +prompt(`Введите ваше число`, '');
+let x = 1;
 
-let userNumberAny = +prompt(`Put your number`, '');
-let isPowerOf3 = Math.log(userNumberAny) / Math.log(3);
-
-if (userNumberAny > 0 && isPowerOf3 % 1 === 0) {
+while (x < userNumberAny) {
+    x *= 3;
+}
+if (x === userNumberAny) {
     console.log(`${userNumberAny} может быть получено путем возведения числа 3 в степень.`);
 } else {
     console.log(`${userNumberAny} Не может быть получено путем возведения числа 3 в степень.`);

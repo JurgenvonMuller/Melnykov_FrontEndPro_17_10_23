44659857678f1d2385c92b9f1cf7a7f0f2ad0688
Yuriy Melnykov <y.melnykov@outlook.com>
2023-11-06 12:59:56 +0100

    1 step

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,1 +1,10 @@
 "use strict";
+
+// Реалізуйте функцію generateKey(length, characters), яка повертає рядок випадкових символів із набору characters довжиною length. span>
+
+// Наприклад:
+
+var characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+var randomChars = generateKey(num, charString)();
+var key = generateKey(16, characters);
+console.log(key); // eg599gb60q926j8i
---
+++ src/js/script.js
@@ -0,0 +1,12 @@
+// Реалізуйте функцію generateKey(length, characters), яка повертає рядок випадкових символів із набору characters довжиною length. span>
+
+// Наприклад:
+
+const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+
+let randomChars = generateKey(num, charString) {
+  
+};
+
+const key = generateKey(16, characters);
+console.log(key); // eg599gb60q926j8i

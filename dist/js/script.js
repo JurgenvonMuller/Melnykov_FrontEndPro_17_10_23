05e4b7ed3c6f7e3d29b19358eb29f9136e640fca
Yuriy Melnykov <y.melnykov@outlook.com>
2023-11-08 22:06:22 +0100

    1 exercise done

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,138 +1,46 @@
 "use strict";
 
-/* ===================== ЗАДАНИЕ ======================================
-  Дан масив об'єктів. Вивести масив телефонних номерів користувачів,
-у яких баланс більше 2000 доларів. 
-І знайти суму всіх балансів користувачів
-*/
-
-var users = [{
-  "index": 0,
-  "isActive": true,
-  "balance": "$2,226.60",
-  "name": "Eugenia Sawyer",
-  "gender": "female",
-  "phone": "+1 (840) 583-3207",
-  "address": "949 John Street, Rose, Puerto Rico, 1857"
-}, {
-  "index": 1,
-  "isActive": true,
-  "balance": "$2,613.77",
-  "name": "Pauline Gallegos",
-  "gender": "female",
-  "phone": "+1 (985) 593-3328",
-  "address": "328 Greenpoint Avenue, Torboy, North Dakota, 6857"
-}, {
-  "index": 2,
-  "isActive": false,
-  "balance": "$3,976.41",
-  "name": "Middleton Chaney",
-  "gender": "male",
-  "phone": "+1 (995) 591-2478",
-  "address": "807 Fleet Walk, Brutus, Arkansas, 9783"
-}, {
-  "index": 3,
-  "isActive": true,
-  "balance": "$1,934.58",
-  "name": "Burns Poole",
-  "gender": "male",
-  "phone": "+1 (885) 559-3422",
-  "address": "730 Seba Avenue, Osage, Alabama, 6290"
-}, {
-  "index": 4,
-  "isActive": true,
-  "balance": "$3,261.65",
-  "name": "Mcfadden Horne",
-  "gender": "male",
-  "phone": "+1 (942) 565-3988",
-  "address": "120 Scholes Street, Kirk, Michigan, 1018"
-}, {
-  "index": 5,
-  "isActive": false,
-  "balance": "$1,790.56",
-  "name": "Suzette Lewis",
-  "gender": "female",
-  "phone": "+1 (837) 586-3283",
-  "address": "314 Dunne Place, Bawcomville, Guam, 9053"
-}];
-var findRichestUsersPhones = function findRichestUsersPhones(arr) {
-  var richUsersBalance = arr.filter(function (obj) {
-    return parseFloat(obj.balance.replace(/[$,]/g, '')) > 2000;
-  });
-  var richUsersPhones = richUsersBalance.map(function (obj) {
-    return obj.phone;
-  });
-  console.log("There are clients phone numbers which has more than 2000 USD\n  ".concat(richUsersPhones));
-  return richUsersPhones;
-};
-var calcTotalUsersMoney = function calcTotalUsersMoney(arr) {
-  // заменяем в объекте строку на число и избавляемся от знака доллара
-  var usersMoneyClearing = arr.map(function (obj) {
-    return parseFloat(obj.balance.replace(/[$,]/g, ''));
-  });
-
-  // Находим сумму всех элементов получившегося массива.
-  var totalUsersMoney = usersMoneyClearing.reduce(function (accu, currentValue) {
-    return accu + currentValue;
-  }, 0);
-
-  // Дробные числа с длинным хвостом  округляем числами с 2 знаками после запятой.
-  totalUsersMoney = Number(totalUsersMoney.toFixed(2));
-  console.log(" Actually all clients has  ".concat(totalUsersMoney, "  USD"));
-  return totalUsersMoney;
-};
-findRichestUsersPhones(users);
-calcTotalUsersMoney(users);
-
-// =========================   NOTES   ++++++++++++++++++++++++++++++++++
-
-// let usersBilansesAndPhones = () => {
-//   let richUsers = [];
-//   let userMoney;
-//   let totalMoney = 0;
-//     for(let obj of users) {
-//       // console.log(obj.balance);
-//       // console.log(obj.phone);
-//       userMoney = parseFloat((obj.balance).replace(/\$|,/g, ''));
-//       totalMoney +=  userMoney;
-//       if(userMoney > 2000) {
-//         richUsers.push(obj.phone);
-//       } 
-//     }
-//   totalMoney = totalMoney.toFixed(2);
-//   // console.log (totalMoney);
-//   // console.log(richUsers);
-
-//   return { totalMoney, richUsers }
-// };
-// let result  = usersBilansesAndPhones();
-// console.log(result.totalMoney);
-// console.log(result.richUsers);
+//        Ця дз складається з чотирьох невеликих завдань, за реалізацію кожної з них можна отримати 25 балів:
 
 /*
-let usersBilansesAndPhones = () => {
-  let richUsers = [];
-  let userMoney;
-  let totalMoney = 0;
-  
-  // Заменяем parseFloat на стрелочную функцию в методе map
-
-  let parsedBalances = users.map(obj => parseFloat((obj.balance).replace(/\$|,/g, '')));
-
-  totalMoney = parsedBalances.reduce((acc, val) => acc + val, 0);
-  
-  richUsers = users.filter(obj => parseFloat((obj.balance).replace(/\$|,/g, '')) > 2000).map(obj => obj.phone);
-
-  
-
-  totalMoney = totalMoney.toFixed(2);
-
-  return { totalMoney, richUsers };
-};
-let result  = usersBilansesAndPhones();
-console.log(result.totalMoney);
-console.log(result.richUsers);
-
+Дано масив з елементами різних типів. Створити функцію яка вираховує середнє арифметичне лише 
+числових елементів даного масиву.
 */
 
-// Дан массив с числами. Используя метод map извлеките из каждого элемента массива квадратный корень и запишите результат в новый массив.
+var a = 12;
+var print = function print() {
+  console.log(a + 'Yes');
+};
+var mixArray = [832, 'defender', print, 23, NaN, 543, a, 987, undefined, 56, true, 456, false, "bignumber"];
+var calcArithmeticMean = function calcArithmeticMean(arr) {
+  var filterNumber = arr.filter(function (item) {
+    return typeof item === 'number' && !isNaN(item);
+  });
+  var summOfNumbers = filterNumber.reduce(function (accu, currentValue) {
+    return accu + currentValue;
+  }, 0);
+  var arithmeticMean = summOfNumbers / filterNumber.length;
+  console.log(filterNumber);
+  console.log(arithmeticMean);
+  return arithmeticMean;
+};
+calcArithmeticMean(mixArray);
+
+/*
+Написати функцію doMath(x, znak, y), яка отримує 3 аргументи: числа x і y,
+ рядок znak. У змінній znak може бути: +, -, *, /, %, ^ (ступінь ).
+ Вивести результат математичної дії, вказаної в змінній znak.Обидва числа і знак
+  виходять від користувача.
+*/
+
+/*
+Написати функцію заповнення даними користувача двомірного масиву.
+Довжину основного масиву і внутрішніх масивів задає користувач. 
+Значення всіх елементів всіх масивів задає користувач.
+*/
+
+/*
+Створити функцію, яка прибирає з рядка всі символи, які ми передали другим аргументом. 
+'func(" hello world", ['l', 'd'])' поверне нам "heo wor". 
+Вихідний рядок та символи для видалення задає користувач.
+*/
--- src/js/script.js
+++ src/js/script.js
@@ -1,207 +1,54 @@
-    /* ===================== ЗАДАНИЕ ======================================
 
-    Дан масив об'єктів. Вивести масив телефонних номерів користувачів,
-    у яких баланс більше 2000 доларів. 
-    І знайти суму всіх балансів користувачів
-    */
+//        Ця дз складається з чотирьох невеликих завдань, за реалізацію кожної з них можна отримати 25 балів:
 
-let users = [
-  {
-  "index": 0,
-  "isActive": true,
-  "balance": "$2,226.60",
-  "name": "Eugenia Sawyer",
-  "gender": "female",
-  "phone": "+1 (840) 583-3207",
-  "address": "949 John Street, Rose, Puerto Rico, 1857"
-  },
-  {
-  "index": 1,
-  "isActive": true,
-  "balance": "$2,613.77",
-  "name": "Pauline Gallegos",
-  "gender": "female",
-  "phone": "+1 (985) 593-3328",
-  "address": "328 Greenpoint Avenue, Torboy, North Dakota, 6857"
-  },
-  {
-  "index": 2,
-  "isActive": false,
-  "balance": "$3,976.41",
-  "name": "Middleton Chaney",
-  "gender": "male",
-  "phone": "+1 (995) 591-2478",
-  "address": "807 Fleet Walk, Brutus, Arkansas, 9783"
-  },
-  {
-  "index": 3,
-  "isActive": true,
-  "balance": "$1,934.58",
-  "name": "Burns Poole",
-  "gender": "male",
-  "phone": "+1 (885) 559-3422",
-  "address": "730 Seba Avenue, Osage, Alabama, 6290"
-  },
-  {
-  "index": 4,
-  "isActive": true,
-  "balance": "$3,261.65",
-  "name": "Mcfadden Horne",
-  "gender": "male",
-  "phone": "+1 (942) 565-3988",
-  "address": "120 Scholes Street, Kirk, Michigan, 1018"
-  },
-  {
-  "index": 5,
-  "isActive": false,
-  "balance": "$1,790.56",
-  "name": "Suzette Lewis",
-  "gender": "female",
-  "phone": "+1 (837) 586-3283",
-  "address": "314 Dunne Place, Bawcomville, Guam, 9053"
-  }
-]
-  
-  
-let findRichestUsersPhones = (arr) => {
 
-  let richUsersBalance =  arr.filter(obj => parseFloat(obj.balance.replace(/[$,]/g, '')) > 2000)
-  let richUsersPhones = richUsersBalance.map(obj => obj.phone);
-  
-  console.log(`There are clients phone numbers which has more than 2000 USD
-  ${richUsersPhones}`);
 
-  return  richUsersPhones
-};
-
-let calcTotalUsersMoney = (arr) => {
-
-  // заменяем в объекте строку на число и избавляемся от знака доллара
-  let usersMoneyClearing = arr.map(obj => parseFloat(obj.balance.replace(/[$,]/g, '')));
-  
-  // Находим сумму всех элементов получившегося массива.
-  let totalUsersMoney = usersMoneyClearing.reduce((accu, currentValue) => accu + currentValue, 0);
-
-  // Дробные числа с длинным хвостом  округляем числами с 2 знаками после запятой.
-  totalUsersMoney = Number(totalUsersMoney.toFixed(2));
-
-  console.log(` Actually all clients has  ${totalUsersMoney}  USD`);
-
-  return totalUsersMoney
-};
-
-findRichestUsersPhones(users);
-
-calcTotalUsersMoney(users);
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// =========================   NOTES   ++++++++++++++++++++++++++++++++++
-
-// let usersBilansesAndPhones = () => {
-//   let richUsers = [];
-//   let userMoney;
-//   let totalMoney = 0;
-//     for(let obj of users) {
-//       // console.log(obj.balance);
-//       // console.log(obj.phone);
-//       userMoney = parseFloat((obj.balance).replace(/\$|,/g, ''));
-//       totalMoney +=  userMoney;
-//       if(userMoney > 2000) {
-//         richUsers.push(obj.phone);
-//       } 
-//     }
-//   totalMoney = totalMoney.toFixed(2);
-//   // console.log (totalMoney);
-//   // console.log(richUsers);
-
-//   return { totalMoney, richUsers }
-// };
-// let result  = usersBilansesAndPhones();
-// console.log(result.totalMoney);
-// console.log(result.richUsers);
 
 /*
-let usersBilansesAndPhones = () => {
-  let richUsers = [];
-  let userMoney;
-  let totalMoney = 0;
-  
-  // Заменяем parseFloat на стрелочную функцию в методе map
-
-  let parsedBalances = users.map(obj => parseFloat((obj.balance).replace(/\$|,/g, '')));
-
-  totalMoney = parsedBalances.reduce((acc, val) => acc + val, 0);
-  
-  richUsers = users.filter(obj => parseFloat((obj.balance).replace(/\$|,/g, '')) > 2000).map(obj => obj.phone);
-
-  
-
-  totalMoney = totalMoney.toFixed(2);
-
-  return { totalMoney, richUsers };
-};
-let result  = usersBilansesAndPhones();
-console.log(result.totalMoney);
-console.log(result.richUsers);
-
+Дано масив з елементами різних типів. Створити функцію яка вираховує середнє арифметичне лише 
+числових елементів даного масиву.
 */
 
+let a = 12;
+let print = () => { console.log(a + 'Yes')};
 
-// Дан массив с числами. Используя метод map извлеките из каждого элемента массива квадратный корень и запишите результат в новый массив.
+let mixArray = [832, 'defender',  print, 23, NaN, 543, a, 987, undefined, 56, true, 456, false, "bignumber"]
 
+let calcArithmeticMean = (arr) => {
+
+  let filterNumber = arr.filter(item => typeof item === 'number' && !isNaN(item));
+
+  let summOfNumbers = filterNumber.reduce((accu, currentValue) => accu + currentValue, 0);
+
+  let arithmeticMean = summOfNumbers / filterNumber.length;
+
+  console.log(filterNumber);
+  console.log(arithmeticMean);
+
+  return arithmeticMean
+};
+calcArithmeticMean(mixArray);
+
+
+
+
+
+
+/*
+Написати функцію doMath(x, znak, y), яка отримує 3 аргументи: числа x і y,
+ рядок znak. У змінній znak може бути: +, -, *, /, %, ^ (ступінь ).
+ Вивести результат математичної дії, вказаної в змінній znak.Обидва числа і знак
+  виходять від користувача.
+*/
+
+/*
+Написати функцію заповнення даними користувача двомірного масиву.
+Довжину основного масиву і внутрішніх масивів задає користувач. 
+Значення всіх елементів всіх масивів задає користувач.
+*/
+
+/*
+Створити функцію, яка прибирає з рядка всі символи, які ми передали другим аргументом. 
+'func(" hello world", ['l', 'd'])' поверне нам "heo wor". 
+Вихідний рядок та символи для видалення задає користувач.
+*/

978e8f96710d38cad2c4a33b1896d8c9bb782766
Yuriy Melnykov <y.melnykov@outlook.com>
2023-11-23 11:52:18 +0100

    фсе

--- src/js/script.js
+++ src/js/script.js
@@ -2,7 +2,9 @@
 
 Вам потрібно зробити конструктор сутності "Студент".
 
-Студент має ім'я, прізвище, рік народження — це властивості. 
+Студент має ім'я, прізвище, рік народження — 
+це властивості. 
+
 Є масив з оцінками, це також властивість. 
 І є можливість отримати вік студента та його середній бал – це методи.
 
@@ -24,37 +26,23 @@
 
 */
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 class Student {
   constructor(firstName, lastName, birthYear) {
     this.firstName = firstName;
     this.lastName = lastName;
     this.birthYear = birthYear;
     this.grades = [];
-    this.attendance = new Array(25).fill(undefined); // Масив відвідуваності, спочатку порожній
+    this.attendance = new Array(25).fill(undefined); // Массив посещения занятий заполняем АНДЕФ
   }
 
   getAge() {
-    const currentYear = new Date().getFullYear();
+    const currentYear = 2023;
     return currentYear - this.birthYear;
   }
 
   getAverageGrade() {
     if (this.grades.length === 0) {
-      return 0; // Якщо немає оцінок, середня оцінка - 0
+      return 0; // пустой массив возвратит 0 так как нет оценок
     }
     const sum = this.grades.reduce((total, grade) => total + grade, 0);
     return sum / this.grades.length;
@@ -70,11 +58,11 @@ class Student {
 
   updateAttendance(status) {
     if (this.attendance.filter(Boolean).length < 25) {
-      // Перевіряємо, чи є місце для нового запису про відвідуваність
+      // Проверка доступного места в массиве
       const emptyIndex = this.attendance.indexOf(undefined);
       this.attendance[emptyIndex] = status;
     } else {
-      console.log('Масив відвідуваності вже заповнений');
+      console.log("Array of attendance is full");
     }
   }
 
@@ -82,31 +70,57 @@ class Student {
     const averageGrade = this.getAverageGrade();
     const attendanceRatio = this.attendance.filter(Boolean).length / 25;
 
-    if (averageGrade > 90 && attendanceRatio > 0.9) {
-      return 'Молодець!';
-    } else if (averageGrade > 90 || attendanceRatio > 0.9) {
-      return 'Добре, але можна краще';
+    if (averageGrade > 90 && attendanceRatio >= 0.9) {
+      return "Молодець!";
+    } else if (averageGrade > 90 || attendanceRatio >= 0.9) {
+      return "Добре, але можна краще";
     } else {
-      return 'Редиска!';
+      return "Редиска!";
     }
   }
 }
 
-// Створюємо екземпляри
-const student1 = new Student('John', 'Doe', 1998);
-const student2 = new Student('Jane', 'Smith', 1999);
-const student3 = new Student('Bob', 'Johnson', 2000);
+// Делаем студентов
+const student1 = new Student("John", "Doe", 1998);
+const student2 = new Student("Jane", "Smith", 1999);
+const student3 = new Student("Bob", "Johnson", 2000);
 
-// Додаємо оцінки та відвідуваність для прикладу
-student1.grades = [95, 85, 92, 88, 97];
-student2.grades = [90, 88, 94, 78, 85];
-student3.grades = [92, 99, 99, 99, 91];
+// Оценки
+student1.grades = [100, 100, 100, 100, 100];
+student2.grades = [100, 80, 100, 100, 100];
+student3.grades = [9, 9, 9, 9, 9, 10];
+
+console.log(student1.getAverageGrade());
+console.log(student2.getAverageGrade());
+
+//Не знаю как не вызывать явно функцию 25 раз на каждое занятие
 
 student1.present();
 student1.present();
-student1.absent();
+student1.present();
+student1.present();
+student1.present();
+student1.present();
+student1.present();
+student1.present();
+student1.present();
+student1.present();
+student1.present();
+student1.present();
+student1.present();
+student1.present();
+student1.present();
+student1.present();
+student1.present();
+student1.present();
+student1.present();
+student1.present();
+student1.present();
+student1.present();
+student1.present();
+student1.present();
 
-// Перевіряємо результати
-console.log('Student 1 summary:', student1.summary());
-console.log('Student 2 summary:', student2.summary());
-console.log('Student 3 summary:', student3.summary());
+//
+console.log(`Student 1 ${student1.firstName}  summary:`, student1.summary());
+console.log(`Student 2 ${student2.firstName}  summary:`, student2.summary());
+console.log(`Student 3 ${student3.firstName}  summary:`, student3.summary());

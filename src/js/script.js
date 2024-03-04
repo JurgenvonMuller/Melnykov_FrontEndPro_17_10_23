758bfb5f6646bc7526b2638fa8c4ddac21339ec1
Yuriy Melnykov <y.melnykov@outlook.com>
2024-03-04 13:21:29 +0100

    Зроблено (пороблено)

--- src/js/script.js
+++ src/js/script.js
@@ -1,25 +1,24 @@
 /*
-У папці images є зображення 1.jpg, 2.jpg, 3.jpg, 4.jpg, 5 .jpg, 6.jpg, 7.jpg, 8.jpg, 9.jpg Вивести зображення з цієї папки, отримане випадковим 
+У папці images є зображення 1.jpg, 2.jpg, 3.jpg, 4.jpg, 5 .jpg, 6.jpg, 7.jpg, 8.jpg, 9.jpg 
+Вивести зображення з цієї папки, отримане випадковим 
 чином (Math.random)
 */
 
-
-
-
     // Создаем массив с названиями изображений
-    var imageNames = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg"];
+    let imageNames = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg"];
 
     // Получаем ссылку на изображение по его id
-    var randomImage = document.getElementById("randomImage");
+    let randomImage = document.getElementById("randomImage");
 
-    // Генерируем случайный индекс для выбора изображения из массива
-    var randomIndex = Math.floor(Math.random() * imageNames.length);
+    // Генерируем случайный индекс для выбора изображения из массива 
+    //умножая случайное число на длину массива и округляя его до наименьшего целого
+    let randomIndex = Math.floor(Math.random() * imageNames.length);
 
     // Получаем случайное изображение из массива
-    var randomImageName = imageNames[randomIndex];
+    let randomImageName = imageNames[randomIndex];
 
-    // Формируем путь к изображению
-    var imagePath = "images/" + randomImageName;
+    // Формируем путь к конкретному изображению:
+    let imagePath = "/src/images/" + randomImageName;
 
     // Устанавливаем путь к случайному изображению в атрибут src тега img
     randomImage.src = imagePath;

87e969b28c4a5dea06a1e37034b20f4a13c1e6ea
Yuriy Melnykov <y.melnykov@outlook.com>
2024-03-04 08:34:09 +0100

    All has done

--- src/js/script.js
+++ src/js/script.js
@@ -1,27 +1,95 @@
-class A {
-  variableA;
+/*Є текстове поле на сторінці. Під час фокусування на цьому полі збоку
+ з'являється <div>. При пропажі фокусу - <div> так само пропадає
+ */
 
-  read() {
-    console.log(`I am a method READ in obj A`);
-  }
-}
+// Получаем ссылки на элементы DOM
+let inputField = document.getElementById("myInput");
+let myDiv = document.getElementById("myDiv");
 
-class B extends A {
-  variableB;
+// Добавляем обработчик события для фокуса на текстовом поле
+inputField.addEventListener("focus", function () {
+  myDiv.style.display = "block";
+});
 
-  close() {
-    console.log(`I am a method CLOSE in obj B`);
-  }
-}
+// Добавляем обработчик события для потери фокуса текстовым полем
+inputField.addEventListener("blur", function () {
+  myDiv.style.display = "none";
+});
 
-let instanceB = new B();
 
-console.log("read" in instanceB);
-console.log("close" in instanceB);
 
-console.log(instanceB.hasOwnProperty("read"));
-console.log(instanceB.hasOwnProperty("close"));
 
-const keys = Object.keys(instanceB);
 
-console.log(keys);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class A {
+//   variableA;
+
+//   read() {
+//     console.log(`I am a method READ in obj A`);
+//   }
+// }
+
+// class B extends A {
+//   variableB;
+
+//   close() {
+//     console.log(`I am a method CLOSE in obj B`);
+//   }
+// }
+
+// let instanceB = new B();
+
+// console.log("read" in instanceB);
+// console.log("close" in instanceB);
+
+// console.log(instanceB.hasOwnProperty("read"));
+// console.log(instanceB.hasOwnProperty("close"));
+
+// const keys = Object.keys(instanceB);
+
+// console.log(keys);

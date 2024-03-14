d36ba88c00b2531ba07f938403d6e88ab7f9bdd3
Yuriy Melnykov <y.melnykov@outlook.com>
2024-03-14 14:13:53 +0100

    nicht

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -15,7 +15,7 @@
   \**************************/
 /***/ (() => {
 
-eval("// Получаем ссылки на DOM-элементы\nvar slider = document.querySelector('.slider');\nvar imagesWrapper = slider.querySelector('.slider__images-wrapper');\nvar images = imagesWrapper.querySelectorAll('.slider__image');\nvar prevButton = slider.querySelector('.slider__arrow_prev');\nvar nextButton = slider.querySelector('.slider__arrow_next');\n\n// Текущий индекс изображения\nvar currentImageIndex = 0;\n\n// Количество изображений\nvar imagesCount = images.length;\n\n// Функция для отображения указанного изображения\nfunction showImage(index) {\n  // Если индекс выходит за пределы количества изображений, возвращаем false\n  if (index < 0 || index >= imagesCount) return false;\n\n  // Скрываем все изображения\n  images.forEach(function (image) {\n    image.style.display = 'none';\n  });\n\n  // Отображаем указанное изображение\n  images[index].style.display = 'block';\n\n  // Обновляем индекс текущего изображения\n  currentImageIndex = index;\n\n  // Возвращаем true\n  return true;\n}\n\n// Отображаем первое изображение\nshowImage(currentImageIndex);\n\n// Функция для перелистывания на следующее изображение\nfunction nextImage() {\n  // Перелистываем на следующее изображение\n  showImage(currentImageIndex + 1);\n}\n\n// Функция для перелистывания на предыдущее изображение\nfunction prevImage() {\n  // Перелистываем на предыдущее изображение\n  showImage(currentImageIndex - 1);\n}\n// Добавляем обработчики событий click на кнопки Next и Prev\nnextButton.addEventListener('click', nextImage);\nprevButton.addEventListener('click', prevImage);\n\n//# sourceURL=webpack:///./src/js/script.js?");
+eval("\n\n//# sourceURL=webpack:///./src/js/script.js?");
 
 /***/ })
 
